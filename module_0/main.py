c754eddf7dbcc697c400e2dd3a7b780c9012e320
Sergey Popinevskiy <gurza000@gmail.com>
2020-04-04 23:27:27 +0300

    Implement own game core using binary search algorithm

--- module_0/main.py
+++ module_0/main.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 import numpy as np
 
 MIN_NUMBER = 1
@@ -44,6 +46,30 @@ def game_core_v2(number):
     return count
 
 
+def game_core_v3(number):
+    """Guesses the secret number (Version 3 - my project).
+
+    Binary search algorithm.
+
+    :param number: the secret number.
+    :type number: int
+    :return: the number of guessing attempts.
+    :rtype: int
+    """
+    min_predict = MIN_NUMBER
+    max_predict = MAX_NUMBER
+    predict = ceil((min_predict + max_predict - 1)/2)
+    count = 1
+    while number != predict:
+        count += 1
+        if number > predict:
+            min_predict = predict + 1
+        else:
+            max_predict = predict - 1
+        predict = ceil((min_predict + max_predict - 1)/2)
+    return count
+
+
 def score_game(game_core):
     """Calculates score for the given game core.
 
@@ -67,4 +93,7 @@ if __name__ == '__main__':
     )
     print('Алгоритм {core} угадывает число в среднем за {score} попыток'.format(
         core=game_core_v2, score=score_game(game_core_v2))
+    )
+    print('Алгоритм {core} угадывает число в среднем за {score} попыток'.format(
+        core=game_core_v3, score=score_game(game_core_v3))
     )
